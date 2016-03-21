1b74b2f5450b49651be635da0481b3768b9ed441
Julian Haagsma <jhaagsma@gmail.com>
2016-03-21 14:34:24 -0400

    fixing docblock

--- Cache.class.php
+++ Cache.class.php
@@ -1,22 +1,35 @@
 <?php
-/*---------------------------------------------------
-These files are part of the empiresPHPframework;
-The original framework core (specifically the mysql.php
-the router.php and the errorlog) was started by Timo Ewalds,
-and rewritten to use APC and extended by Julian Haagsma,
-for use in Earth Empires (located at http://www.earthempires.com );
-it was spun out for use on other projects.
 
-The general.php contains content from Earth Empires
-written by Dave McVittie and Joe Obbish.
-
-
-The example website files were written by Julian Haagsma.
-
-All files are licensed under the MIT License.
-
-First release, September 3, 2012
----------------------------------------------------*/
+/**
+ * This is the index, which handles all the incoming stuff
+ *
+ * PHP version 5
+ *
+ * ------
+ * These files are part of the eemphyre framework;
+ * Formerly: emPHyre, and empiresPHPframework
+ * The original framework core (specifically the mysql.php
+ * the router.php and the errorlog) was started by Timo Ewalds,
+ * and rewritten to use APC and extended by Julian Haagsma,
+ * for use in Earth Empires (located at http://www.earthempires.com );
+ * it was spun out for use on other projects.
+ *
+ * The general.php contains content from Earth Empires
+ * written by Dave McVittie and Joe Obbish.
+ *
+ * The example website files were written by Julian Haagsma.
+ *
+ * @category Index
+ * @package  eemphyre: examplesite
+ * @author   Julian Haagsma <jhaagsma@gmail.com>
+ * @author   Timo Ewalds <tewalds@gmail.com>
+ * @author   Dave McVittie <dave.mcvittie@gmail.com>
+ * @author   Joe Obbish <slagpit@earthempires.com>
+ * @license  All files are licensed under the MIT License.
+ * @link     https://github.com/jhaagsma/eemphyre
+ * @link     http://eemphyre.com
+ * @since    Pulled out of errorlog.php 2016-03-15
+ */
 
 namespace EmPHyre;
 
