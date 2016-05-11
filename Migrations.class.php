9f5ad116cb8080beb88f40cac54ab977acd6c421
Julian Haagsma <jhaagsma@gmail.com>
2016-05-11 11:19:19 -0600

    adding Migrations class

---
+++ Migrations.class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Migrations is the migration abstract class for migrations
+ *
+ * PHP version 7
+ *
+ * ------
+ * These files are part of the empiresPHPframework;
+ * The original framework core (specifically the mysql.php
+ * the router.php and the errorlog) was started by Timo Ewalds,
+ * and rewritten to use APC and extended by Julian Haagsma,
+ * for use in Earth Empires (located at http://www.earthempires.com );
+ * it was spun out for use on other projects.
+ *
+ * The general.php contains content from Earth Empires
+ * written by Dave McVittie and Joe Obbish.
+ *
+ * The example website files were written by Julian Haagsma.
+ *
+ * @category Core
+ * @package  EmPHyre
+ * @author   Julian Haagsma <jhaagsma@gmail.com>
+ * @license  All files are licensed under the MIT License.
+ * @link     https://github.com/jhaagsma/emPHyre
+ * @since    Pulled out of PHPRouter.class.php 2016-03-15
+ */
+namespace EmPHyre;
+
+abstract class Migrations
+{
+    protected static $db; //the database
+
+    public static function db($db = null)
+    {
+        static::$db = Container::getDb();
+    }
+
+    abstract public function up();
+    abstract public function down();
+}
