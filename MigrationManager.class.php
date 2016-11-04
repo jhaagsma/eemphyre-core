3e91a02ca2a7aefc04e6a35022c3d062e903106a
Julian Haagsma <jhaagsma@gmail.com>
2016-11-04 13:45:09 -0600

    editing docblock

--- MigrationManager.class.php
+++ MigrationManager.class.php
@@ -1,25 +1,30 @@
-<?php
+<?php namespace EmPHyre;
+
 /**
- * MigrationManager for PhaseWeb project
+ * ABSTRACT CLASS, Migration Manager object for the EmPHyre project
  *
- * PHP version 5
+ * PHP version 7
  *
  * ------
- * These files are part of the PhaseWeb project;
- * This project uses the EmPHyre microframework,
- * and is built off the EmPHyre example files
+ * These files are part of the empiresPHPframework;
+ * The original framework core (specifically the mysql.php
+ * the router.php and the errorlog) was started by Timo Ewalds,
+ * and rewritten to use APC and extended by Julian Haagsma,
+ * for use in Earth Empires (located at http://www.earthempires.com );
+ * it was spun out for use on other projects.
  *
- * Written for PhaseSensors Julian Haagsma.
+ * The general.php contains content from Earth Empires
+ * written by Dave McVittie and Joe Obbish.
  *
- * @category Classes
- * @package  PhaseWeb
+ * The example website files were written by Julian Haagsma.
+ *
+ * @category Core
+ * @package  EmPHyre
  * @author   Julian Haagsma <jhaagsma@gmail.com>
  * @license  All files are licensed under the MIT License.
- * @link     https://demo3.phasesensors.com
- * @since    PhaseWeb was created 2014-09, modernized to current in 2016-03
+ * @link     https://github.com/jhaagsma/emPHyre
+ * @since    Recombined from empiresPHPframework extensions on Nov 4 2016
  */
-
-namespace EmPHyre;
 
 abstract class MigrationManager
 {
