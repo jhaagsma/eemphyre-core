a512965e9491c036b89c0fd09e1b85e9ea15305b
Julian Haagsma <jhaagsma@gmail.com>
2016-11-18 15:38:07 -0700

    add M2M

---
+++ M2M.class.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * M2M is the Many-to-Many base class to extend for various
+ * classes and objects
+ *
+ * PHP version 7
+ *
+ * ------
+ * These files are part of the empiresPHPframework;
+ * The original framework core (specifically the mysql.php
+ * the router.php and the errorlog) was started by Timo Ewalds,
+ * and rewritten to use APC and extended by Julian Haagsma,
+ * for use in Earth Empires (located at http://www.earthempires.com );
+ * it was spun out for use on other projects.
+ *
+ * The general.php contains content from Earth Empires
+ * written by Dave McVittie and Joe Obbish.
+ *
+ * The example website files were written by Julian Haagsma.
+ *
+ * @category Core
+ * @package  EmPHyre
+ * @author   Julian Haagsma <jhaagsma@gmail.com>
+ * @license  All files are licensed under the MIT License.
+ * @link     https://github.com/jhaagsma/emPHyre
+ * @since    November 2016
+ */
+
+namespace EmPHyre;
+
+class M2M
+{
+    protected static $db; //the database
+    protected static $_table_name = null;
+    protected static $_primary_key_1 = 'id1';
+    protected static $_primary_key_2 = 'id2';
+    protected $_data;
+
+    public function __construct($table_name, $primary_key_1 = 'id1', $primary_key_2 = 'id2')
+    {
+        static::$_table_name = $table_name;
+        static::$_primary_key_1 = $primary_key_1;
+        static::$_primary_key_2 = $primary_key_2;
+        $this->initialize();
+    }
+
+    public function setDb($db = null)
+    {
+        if ($db == null) {
+            static::$db = Container::getDb();
+        }
+        static::$db = $db;
+    }
+
+    //simple version for basic classes
+    public static function db()
+    {
+        if (!static::$db) {
+            static::$db = Container::getDb();
+        }
+    }
+
+    public function initialize()
+    {
+        if (!static::$_table_name) {
+            trigger_error('TABLE NAME NOT SET IN ' . get_class($this));
+        }
+    }
+
+    public function getM2M($pk_value = 0, $use_pk2 = false)
+    {
+        static::db();
+        return static::$db->pquery(
+            "SELECT `".
+            ($use_pk2 ? static::$_primary_key_1 : static::$_primary_key_2 ).
+            "` FROM `".static::$_table_name."` WHERE `".
+            ($use_pk2 ? static::$_primary_key_2 : static::$_primary_key_1 ).
+            "`=?",
+            $pk_value
+        )->fetchFieldSet();
+    }
+}
