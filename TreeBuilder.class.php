0bbd4dbb98601041c4aa388a47daeb36a5a82b68
qzjul <jhaagsma@gmail.com>
2018-02-11 12:11:19 -0700

    add tree builder

---
+++ TreeBuilder.class.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * TreeBuilder builds the tree for the routing object
+ *
+ * PHP version 7
+ *
+ * ------
+ * These files are part of the empiresPHPframework;
+ * The original framework core (specifically the mysql.php
+ * the router.php and the errorlog) was started by Timo Ewalds,
+ * and rewritten to use APC and extended by Julian Haagsma,
+ * for use in Earth Empires (located at http://www.earthempires.com );
+ * it was spun out for use on other projects.
+ *
+ * The general.php contains content from Earth Empires
+ * written by Dave McVittie and Joe Obbish.
+ *
+ * The example website files were written by Julian Haagsma.
+ *
+ * @category Core
+ * @package  EmPHyre
+ * @author   Julian Haagsma <jhaagsma@gmail.com>
+ * @author   Timo Ewalds <tewalds@gmail.com>
+ * @license  All files are licensed under the MIT License.
+ * @link     https://github.com/jhaagsma/emPHyre
+ * @since    Feb 11, 2018
+ */
+
+namespace EmPHyre;
+
+class TreeBuilder
+{
+    
+}//end class
