4745be592b80154d605a9d0356b4f7d426aa3f1f
qzjul <jhaagsma@gmail.com>
2018-02-16 17:20:45 -0700

    add 404 class

---
+++ FourOhFour.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * FourOhFour is the 404 not found class
+ *
+ * PHP version 7
+ *
+ * ------
+ * These files are part of the empiresPHPframework;
+ * The original framework core (specifically the mysql.php
+ * the router.php and the errorlog) was started by Timo Ewalds,
+ * and rewritten to use APC and extended by Julian Haagsma,
+ * for use in Earth Empires (located at http://www.earthempires.com );
+ * it was spun out for use on other projects.
+ *
+ * The general.php contains content from Earth Empires
+ * written by Dave McVittie and Joe Obbish.
+ *
+ * The example website files were written by Julian Haagsma.
+ *
+ * @category Core
+ * @package  EmPHyre
+ * @author   Julian Haagsma <jhaagsma@gmail.com>
+ * @author   Timo Ewalds <tewalds@gmail.com>
+ * @license  All files are licensed under the MIT License.
+ * @link     https://github.com/jhaagsma/emPHyre
+ * @since    Feb 11, 2018
+ */
+
+namespace EmPHyre;
+
+class FourOhFour
+{
+    public static function notFound($data, $path, $user)
+    {
+        //just include /404.php for now
+        include_once(getenv('DOCUMENT_ROOT')."/404.php");
+    }
+}//end class
