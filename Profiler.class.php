5864a351c8323efdf16ee569aef2b43a13ad81e5
qzjul <jhaagsma@gmail.com>
2018-02-24 16:52:50 -0700

    add Profiler

---
+++ Profiler.class.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This is the Profiler for the EmPHyre project
+ *
+ * PHP version 7
+ *
+ * ------
+ * These files are part of the empiresPHPframework;
+ * The original framework core (specifically the mysql.php
+ * the router.php and the errorlog) was started by Timo Ewalds,
+ * and rewritten to use APC and extended by Julian Haagsma,
+ * for use in Earth Empires (located at http://www.earthempires.com );
+ * it was spun out for use on other projects.
+ *
+ * The general.php contains content from Earth Empires
+ * written by Dave McVittie and Joe Obbish.
+ *
+ * The example website files were written by Julian Haagsma.
+ *
+ * @category Core
+ *
+ * @package  EmPHyre
+ *
+ * @author   Julian Haagsma <jhaagsma@gmail.com>
+ *
+ * @license  All files are licensed under the MIT License.
+ *
+ * @link     https://github.com/jhaagsma/emPHyre
+ *
+ * @since    February 24, 2018
+ */
+
+namespace EmPHyre;
+
+/**
+ * The profiling class
+ */
+class Profiler
+{
+    private $time_start = 0;
+    private $timers     = [];
+
+    /**
+     * Set the start time
+     *
+     * @param integer $start The start time
+     *
+     * @return null
+     */
+    public static function setStart($start = 0)
+    {
+        self::$time_start = $start;
+    }//end setStart()
+
+    /**
+     * Add another timer line item
+     *
+     * @param  string $timer The line item to add
+     *
+     * @return null
+     */
+    public static function profile($timer)
+    {
+        self::$timers[$timer] = self::codetime(true);
+    }//end profile()
+
+    /**
+     * Find how long it's been since the start
+     *
+     * @param  boolean $detail Whether or not we want fractions of a ms
+     *
+     * @return folat           A time
+     */
+    private function codetime($detail = false)
+    {
+        if ($detail) {
+            return (microtime(true) - self::$time_start) * 1000;
+        } else {
+            return ceil((microtime(true) - self::$time_start) * 1000);
+        }
+    }//end codetime()
+}//end class
